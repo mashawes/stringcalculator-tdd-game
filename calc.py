f53c4f96efa26e65164a9db985b551819b03e073
MohidSheraz <msheraz@andrew.cmu.edu>
2026-06-10 17:13:15 +0300

    GREEN: parse custom delimiter from header

--- calc.py
+++ calc.py
@@ -1,4 +1,16 @@
 """ Edit the function below to implement the String Calculator TDD Kata """
 
+import re
+
 def add(numbers):
-    return "42"
+    if numbers == "":
+        return 0
+
+    delimiter = r",|\n"
+
+    if numbers.startswith("//"):
+        header, numbers = numbers.split("\n", 1)
+        delimiter = re.escape(header[2:])
+
+    parts = re.split(delimiter + r"|,|\n", numbers)
+    return sum(int(n) for n in parts if n != "")
